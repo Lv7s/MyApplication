dcfd55c231a7a25f07dbcbf563f4126ed769f31a
Luis Valverde <luvalfer2009@hotmail.com>
2022-09-03 19:11:14 -0300

    terminada interfaz hepes 4 geles sds

--- app/src/main/java/com/lv7s/myapplication/gelsdspage.kt
+++ app/src/main/java/com/lv7s/myapplication/gelsdspage.kt
@@ -5,27 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.lv7s.myapplication.databinding.FragmentGelsdspageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [gelsdspage.newInstance] factory method to
- * create an instance of this fragment.
- */
 class gelsdspage : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var _binding : FragmentGelsdspageBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -33,27 +22,18 @@ class gelsdspage : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gelsdspage, container, false)
+        _binding = FragmentGelsdspageBinding.inflate(inflater, container, false)
+        binding.tosdsstacking.setOnClickListener {
+            findNavController().navigate(R.id.action_gelsdspage_to_gelsdspagestack)
+        }
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment gelsdspage.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            gelsdspage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
--- app/src/main/java/com/lv7s/myapplication/gelsdspagestack.kt
+++ app/src/main/java/com/lv7s/myapplication/gelsdspagestack.kt
@@ -5,11 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.lv7s.myapplication.databinding.FragmentInicioBinding
+import com.lv7s.myapplication.databinding.FragmentGelsdspageBinding
+import com.lv7s.myapplication.databinding.FragmentGelsdspagestackBinding
 
-class Inicio : Fragment() {
-    private var _binding : FragmentInicioBinding? = null
+class gelsdspagestack : Fragment() {
+    private var _binding : FragmentGelsdspagestackBinding? = null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +22,8 @@ class Inicio : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentInicioBinding.inflate(inflater, container, false)
-        binding.toBuffer.setOnClickListener {
-            findNavController().navigate(R.id.action_inicio_to_buffer)
-        }
-        binding.toGelsds.setOnClickListener {
-            findNavController().navigate(R.id.action_inicio_to_gelsdspage)
-        }
+        _binding = FragmentGelsdspagestackBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onDestroyView() {
