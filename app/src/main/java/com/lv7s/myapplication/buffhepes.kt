54f51203c322b6faf4b9529145ce824eb1958a8a
Luis Valverde <luvalfer2009@hotmail.com>
2022-09-03 17:14:21 -0300

    preparando buffer HEPES

--- app/src/main/java/com/lv7s/myapplication/buffhepes.kt
+++ app/src/main/java/com/lv7s/myapplication/buffhepes.kt
@@ -5,16 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.lv7s.myapplication.databinding.FragmentBufferBinding
+import com.lv7s.myapplication.databinding.FragmentBuffhepesBinding
 
 
-class Buffer : Fragment() {
-    private var _binding : FragmentBufferBinding? = null
+
+class buffhepes : Fragment() {
+    private var _binding : FragmentBuffhepesBinding? = null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+
         }
     }
 
@@ -22,12 +23,7 @@ class Buffer : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentBufferBinding.inflate(inflater, container, false)
-        binding.btntoBufferTRIS.setOnClickListener {
-            findNavController().navigate(R.id.action_buffer_to_bufftris)}
-        binding.btntoBufferHEPES.setOnClickListener {
-            findNavController().navigate(R.id.action_buffer_to_buffhepes)
-        }
+        _binding = FragmentBuffhepesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -35,7 +31,6 @@ class Buffer : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
     companion object {
 
     }
