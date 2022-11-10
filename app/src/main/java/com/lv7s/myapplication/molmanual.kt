75427fbf1e292ec01ca95ae013ea753a700ac827
Luis Valverde <luvalfer2009@hotmail.com>
2022-11-10 11:30:16 -0300

    terminado molaridad manual

--- app/src/main/java/com/lv7s/myapplication/molmanual.kt
+++ app/src/main/java/com/lv7s/myapplication/molmanual.kt
@@ -1,31 +1,25 @@
 package com.lv7s.myapplication
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lv7s.myapplication.databinding.FragmentMolmanualBinding
+import java.math.RoundingMode
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [molmanual.newInstance] factory method to
- * create an instance of this fragment.
- */
 class molmanual : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    var numPesMol : Float? = null
+    var numVolLit : Float? = null
+    var numMolFinal : Float? = null
+    private var _binding : FragmentMolmanualBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -33,27 +27,44 @@ class molmanual : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_molmanual, container, false)
+        _binding = FragmentMolmanualBinding.inflate(inflater, container, false)
+        var respuestagramosapesar = binding.rptagramosPesar
+        binding.btncalcgrampes.setOnClickListener {
+            if  (TextUtils.isEmpty(binding.valPesMol.text.toString())){
+                binding.valPesMol.error = "Ingresa un numero"
+                binding.valPesMol.requestFocus()
+                return@setOnClickListener
+            }else {
+                numPesMol = binding.valPesMol.text.toString().toFloat()
+            }
+
+            if  (TextUtils.isEmpty(binding.valVolLit.text.toString())){
+                binding.valVolLit.error = "Ingresa un numero"
+                binding.valVolLit.requestFocus()
+                return@setOnClickListener
+            }else {
+                numVolLit = binding.valVolLit.text.toString().toFloat()
+            }
+
+            if  (TextUtils.isEmpty(binding.valMolFinal.text.toString())){
+                binding.valMolFinal.error = "Ingresa un numero"
+                binding.valMolFinal.requestFocus()
+                return@setOnClickListener
+            }else {
+                numMolFinal = binding.valMolFinal.text.toString().toFloat()
+            }
+            val resultadogramosapesar =  numMolFinal!!.times(numPesMol!!*numVolLit!!).toBigDecimal().setScale(2, RoundingMode.UP).toString()
+            respuestagramosapesar.setText(resultadogramosapesar)
+        }
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment molmanual.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            molmanual().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
